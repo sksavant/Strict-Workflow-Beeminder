09a8f4ebd48843c9e34c95c9e323b3b393f444d3
Maggie Delano <maggie.delano@gmail.com>
2015-07-10 19:38:05 -0400

    Displays the number of pomodoros completed for the default goal if no tag, and for the #goal in the comment if present.

--- popup.js
+++ popup.js
@@ -14,5 +14,13 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
 	if (message.displayComment) {
 		document.getElementById('comment').value = message.displayComment;
 	}
+	if (message.value) {
+		if (message.value == 1){
+			document.getElementById('doneToday').innerHTML = "You have completed " + message.value + " pomodoro today.";	
+		}
+		else {
+			document.getElementById('doneToday').innerHTML = "You have completed " + message.value + " pomodoros today.";
+		}
+	}
 
 });
